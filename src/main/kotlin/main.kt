e026e85b6f66a80d228fd7d5c341a27cc3e6acf2
Sergey Rostov <sergey.rostov@jetbrains.com>
2022-05-30 16:43:36 +0200

    repro2

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,38 +1,72 @@
-import kotlinx.coroutines.CopyableThreadContextElement
-import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.*
 import java.util.concurrent.atomic.AtomicInteger
 import kotlin.coroutines.AbstractCoroutineContextElement
 import kotlin.coroutines.CoroutineContext
 
-val lastId = AtomicInteger()
+private val lastId = AtomicInteger()
 
-val currentCoroutine: ThreadLocal<CurrentThread?> = object : ThreadLocal<CurrentThread?>() {
-    override fun initialValue(): CurrentThread? = null
+private val currentCoroutine: ThreadLocal<CoroutineCurrentThread?> = object : ThreadLocal<CoroutineCurrentThread?>() {
+    override fun initialValue(): CoroutineCurrentThread? = null
 }
 
 @OptIn(DelicateCoroutinesApi::class, ExperimentalCoroutinesApi::class)
-class CurrentThread(private val coroutineId: Int) : CopyableThreadContextElement<Unit>,
+class CoroutineCurrentThread(val coroutineId: Int) : CopyableThreadContextElement<Unit>,
     AbstractCoroutineContextElement(Key) {
 
+    var i = 0
+
     override fun copyForChild(): CopyableThreadContextElement<Unit> =
-        CurrentThread(lastId.incrementAndGet())
+        newTrackedCoroutine()
 
     override fun mergeForChild(overwritingElement: CoroutineContext.Element): CoroutineContext =
-        CurrentThread(lastId.incrementAndGet())
+        newTrackedCoroutine()
 
     override fun updateThreadContext(context: CoroutineContext) {
-        check(currentCoroutine.get() == null) // this check is failed
+//        check(currentCoroutine.get() == null) // this check is failed
         currentCoroutine.set(this)
+        i++
+        println("thread ${Thread.currentThread().id}: coroutine $coroutineId executing $i")
     }
 
     override fun restoreThreadContext(context: CoroutineContext, oldState: Unit) {
         currentCoroutine.set(null)
+        println("thread ${Thread.currentThread().id}: coroutine $coroutineId suspended $i")
     }
 
     override fun toString(): String {
         return "CurrentThread(coroutineId=$coroutineId)"
     }
 
-    companion object Key : CoroutineContext.Key<CurrentThread>
+    companion object Key : CoroutineContext.Key<CoroutineCurrentThread>
+}
+
+fun newTrackedCoroutine() =
+    CoroutineCurrentThread(lastId.incrementAndGet())
+
+fun printState(str: String) {
+    val coroutine = currentCoroutine.get()
+    println("$str (thread ${Thread.currentThread().id}, coroutine-${coroutine?.coroutineId}, run ${coroutine?.i})")
+}
+
+fun main() {
+    runBlocking(newTrackedCoroutine() + Dispatchers.Unconfined) {
+        (1..2).map {
+            launch {
+                printState("$it: A")
+                delay(10)
+                printState("$it: B")
+                runBlocking(Dispatchers.Unconfined) {
+                    printState("$it: C")
+                    delay(10)
+                    printState("$it: D")
+                }
+                printState("$it: E")
+                delay(10)
+                printState("$it: F")
+            }
+        }.forEach {
+            it.join()
+            printState("$it: G")
+        }
+    }
 }
